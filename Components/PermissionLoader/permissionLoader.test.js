135b808ee8b9c51d1f171a37856bea05f57b9d83
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-12-12 13:41:59 +0200

    updated PermissionLoader

--- Components/PermissionLoader/permissionLoader.test.js
+++ Components/PermissionLoader/permissionLoader.test.js
@@ -1,17 +1,18 @@
 import PermissionLoader from "./PermissionLoader";
-import renderer from 'react-test-renderer';
+import renderer from "react-test-renderer";
 
-describe("PermissionLoader", function(){
-    const loader = renderer.create(
-            <PermissionLoader />
-    ).toJSON();
-    it("should contain a view element", function(){
-        expect(loader.type).toBe("View");
-    });
-    it("should contain a View with one child", function(){
-        expect(loader.children.length).toBe(1);
-    });
-    it("should contain a loader of size 42", function(){
-        expect(loader.children[0].props.size).toBe(42);
-    });
+describe("PermissionLoader", function () {
+  const loader = renderer.create(<PermissionLoader />).toJSON();
+  it("should contain a view element", function () {
+    expect(loader.type).toBe("View");
+  });
+  it("should contain a View with one child", function () {
+    expect(loader.children.length).toBe(1);
+  });
+  it("should contain a loader of size 42", function () {
+    expect(loader.children[0].props.size).toBe(42);
+  });
+  it("should render the PermissionLoader component correctly", () => {
+    expect(loader).toMatchSnapshot();
+  });
 });

7e427990beed25199fb44fcd05e2e1add22e90f7
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-12-12 13:40:00 +0200

    updated ContactInfoHead

--- Components/ContactInfoHead/contactInfoHead.test.js
+++ Components/ContactInfoHead/contactInfoHead.test.js
@@ -1,11 +1,22 @@
-import renderer from 'react-test-renderer';
-import ContactInfoHead from './ContactInfoHead';
+import renderer from "react-test-renderer";
+import ContactInfoHead from "./ContactInfoHead";
 
-describe("ContactInfoHead", function(){
-    const contactInfo = renderer.create(
-            <ContactInfoHead profilePic="noContact" name="Jake" status="available" statusColor="purple"/>
-    ).toJSON();
-    it("should have three children", function(){
-        expect(contactInfo.children.length).toBe(3);
-    });
+describe("ContactInfoHead", function () {
+  const contactInfo = renderer
+    .create(
+      <ContactInfoHead
+        profilePic='noContact'
+        name='Jake'
+        status='available'
+        statusColor='purple'
+      />
+    )
+    .toJSON();
+  it("should have three children", function () {
+    expect(contactInfo.children.length).toBe(3);
+  });
+
+  it("should render the contactInfo component correctly", () => {
+    expect(contactInfo).toMatchSnapshot();
+  });
 });
